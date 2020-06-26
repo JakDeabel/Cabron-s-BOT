0979d7296bfa623464284eb8f7544681619c4186
Beershock <44744872+JakDeabel@users.noreply.github.com>
2020-06-26 15:11:05 +0100

    Update ping.js

--- comandos/ping.js
+++ comandos/ping.js
@@ -1,11 +1,17 @@
 const { MessageEmbed } = require('discord.js');
 
-exports.run = (bot, message, args) => {
+module.exports = {
+    name: 'ping',
+    aliases: ['p'],
+    description: 'Show ping latency',
+    cooldown: 5,
+    run: async (client, message) => {
 
-const m = await message.channel.send(`Ping`);
-let embed = new MessageEmbed()
-.setDescription(`Pong!\n🚩 Latency ${Math.floor(m.createdTimestamp - message.createdTimestamp)}\n🌐 Client ${client.ws.ping}`)
-.setFooter('¯\_(ツ)_/¯')
-message.channel.send(embed)
-await m.delete();
+        const m = await message.channel.send(`Ping`);
+        let embed = new MessageEmbed()
+        .setDescription(`Pong!\n🚩 Latency ${Math.floor(m.createdTimestamp - message.createdTimestamp)}\n🌐 Client ${client.ws.ping}`)
+        .setFooter('¯\_(ツ)_/¯')
+        message.channel.send(embed)
+        await m.delete();
+    }
 }
