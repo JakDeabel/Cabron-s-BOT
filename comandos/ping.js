ee209c1ca038b6f64a8f9cf2f52230e9b2213506
Beershock <44744872+JakDeabel@users.noreply.github.com>
2020-06-26 15:14:14 +0100

    Update ping.js

--- comandos/ping.js
+++ comandos/ping.js
@@ -5,11 +5,11 @@ module.exports = {
     aliases: ['p'],
     description: 'Show ping latency',
     cooldown: 5,
-    run: async (client, message) => {
+    run: async (bot, message) => {
 
         const m = await message.channel.send(`Ping`);
         let embed = new MessageEmbed()
-        .setDescription(`Pong!\n🚩 Latency ${Math.floor(m.createdTimestamp - message.createdTimestamp)}\n🌐 Client ${client.ws.ping}`)
+        .setDescription(`Pong!\n🚩 Latency ${Math.floor(m.createdTimestamp - message.createdTimestamp)}\n🌐 Client ${bot.ws.ping}`)
         .setFooter('¯\_(ツ)_/¯')
         message.channel.send(embed)
         await m.delete();
