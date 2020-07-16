d1fc54b1824cffd61070460e76ddd483f8a0bd52
Beershock <44744872+JakDeabel@users.noreply.github.com>
2020-07-16 18:30:55 +0100

    Suggestion command add

---
+++ comandos/sugestao.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+//👨 Masculino 👩 Feminino 🌈 LGBT
+exports.run = (bot, message, args) => {
+  //if(!message.member.roles.some(r => r.name === "👨 Masculino") || !message.member.roles.some(r => r.name === "👩 Feminino") || !message.member.roles.some(r => r.name === "🌈 LGBT")) {
+    let suggestmessage = args.join(" ")//.slice(22);
+    let suggestchannel = message.guild.channels.find(`name`, "『📖』sugestoes");
+
+    if (!suggestchannel) {
+        return message.reply("<a:702223671066099812:711253483067801631> **|** Não detetei o canal de sugestões!")
+    }
+
+    if (!suggestmessage) {
+        return message.reply("<a:702223671066099812:711253483067801631> **|** Por favor sugira algo!")
+    }
+
+    let embed = new Discord.RichEmbed()
+        .addField("**Sugestão Recebida**", `${suggestmessage}`)
+        .setFooter(`Sugestão de ${message.author.tag}`)
+        .setTimestamp()
+    suggestchannel.send({
+        embed
+    })/*.then(msg => {
+        msg.react('✅').then(r => msg.react('❎'))
+    });*/
+
+
+    message.reply(`<a:711037344798474260:711251952536780802> | Sugestão enviada.`)
+    return;
+  //} else {
+    //message.reply('Por favor registre-se em #🐦 • registro para usar este comando.')
+  //}
+}
