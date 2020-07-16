2b7f3f55d4f3f58f5f38d67a4df2c82122afe80f
Beershock <44744872+JakDeabel@users.noreply.github.com>
2020-07-16 19:20:47 +0100

    Create anuncio.js

---
+++ comandos/anuncio.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client();
+
+exports.run = (client, message, args) => {  
+  if (message.author.id !== '340622268424126465' && message.author.id !== '318149637964038144' && message.author.id !== '380427905995636747' && !client.config.owners.includes(message.author.id)) return; 
+  
+  const filter = m => m.author.id === message.author.id
+  
+    message.reply('> <:JuiceBox:710121833885860040> Qual vai ser o **título** do seu anúncio?').then(async msg1 => {
+      let at = msg1.channel.createMessageCollector(filter , {
+      max: 1
+    })
+    
+  at.on("collect", () => {
+    
+    message.reply('> <:JuiceBox:710121833885860040> Qual vai ser o **conteúdo** do seu anúncio?').then(async msg2 => {
+      let ac = msg2.channel.createMessageCollector(filter , {
+      max: 1
+    })
+      
+  ac.on("collect", () => {
+     
+       let canala = message.guild.channels.find(`id`, "709473791440584794");
+       let embeda = new Discord.MessageEmbed()
+         .setTitle(`${at.collected.first().content}`)
+         .setDescription(`${ac.collected.first().content}`)
+         .setColor('#4a2496')
+         .setTimestamp()
+       
+       canala.send(embeda)   
+       })
+     })      
+   })
+ })
+}
