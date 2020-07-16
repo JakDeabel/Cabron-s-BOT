de4e94bc53fa95e29085cbb19ab2ee16d739481d
Beershock <44744872+JakDeabel@users.noreply.github.com>
2020-07-16 18:09:05 +0100

    Update parceria.js

--- comandos/parceria.js
+++ comandos/parceria.js
@@ -8,7 +8,6 @@ exports.run = (client, message, args) => {
   
   const canal = client.channels.get("709968517981798504");
 
-  const reason = args.slice(1).join(' ');
   
   const sayMessage = args.join(" ");
       message.delete().catch();
@@ -16,9 +15,7 @@ exports.run = (client, message, args) => {
   const embed = new Discord.RichEmbed()  
     .setTitle('<:4015_my_verified_emoji:710118751936184361> Nova parceria oficial do Cabrons!')
     .setDescription(`${sayMessage}`)
-    .setDescription(`Convite: ${reason}`)
     .setColor('#4a2496')
-
     .setFooter('© Cabrons - Parcerias')
   canal.send(embed);
 }
