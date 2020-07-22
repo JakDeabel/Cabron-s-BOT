3dcde0ddae3ff60ef84fd7f4f5347e3e4688f8c0
Glitch (low-flying-son) <none>
2020-07-22 05:00:07 +0000

    🌆🍓 Updated with Glitch

--- index.js
+++ index.js
@@ -7,16 +7,7 @@ const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
 bot.aliases = new Discord.Collection();
 
-bot.on("ready", () => {
 
-    var jogando = `c!help`
-    console.log("Conectado!!")
-    console.log(`Servidores(${bot.guilds.size}):\n${bot.guilds.map(servidor => servidor.name).join(", ")}`)
-    
-    bot.user.setGame(jogando);
-   
-
-})
 
 
 
@@ -44,33 +35,20 @@ fs.readdir("./commands/", (err, files) => {
 
 
 
+ 
+  bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
+  bot.user.setActivity(`c!help`);
+  bot.user.setStatus('online');
+
   bot.on("message", async message => {
     if(message.author.bot) return;
     if(message.channel.type === "dm") return;
-    
-    let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
-    if(!prefixes[message.guild.id]){
-        prefixes[message.guild.id] = {
-            prefixes: botconfig.prefix
-        };
-    }
- 
-    let prefix = prefixes[message.guild.id].prefixes;
- 
-    if(!message.content.startsWith(prefix)) return; //responde só ao seu prefixo
-    let messageArray = message.content.split(" ");
-    let command = messageArray[0];
-    let args = messageArray.slice(1);
- 
-    let arquivocmd = bot.commands.get(command.slice(prefix.length));
-    if(arquivocmd) arquivocmd.run(bot,message,args);
-});
+    let prefix = botconfig.prefix
     let messageArray = message.content.split(" ");
     let args = message.content.slice(prefix.length).trim().split(/ +/g);
     let cmd = args.shift().toLowerCase();
     let commandfile;
-    
-   
 
     if (bot.commands.has(cmd)) {
       commandfile = bot.commands.get(cmd);
@@ -86,7 +64,7 @@ fs.readdir("./commands/", (err, files) => {
   
   } catch (e) {
   }}
-  )
+  )})
 
 
 bot.login(process.env.token)
