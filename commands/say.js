c60088e8a3e4ba54fd558e2cd20ecfdbbe42000d
Glitch (low-flying-son) <none>
2020-07-22 06:15:10 +0000

    🏩🕷 Updated with Glitch

--- commands/say.js
+++ commands/say.js
@@ -1,6 +1,10 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+  
+  if(!message.member.roles.has('709515494633504798') && !message.member.roles.has('709515494633504798')) {    
+}
+  
       const sayMessage = args.join(" ");
       message.delete().catch();
       message.channel.send(sayMessage);
