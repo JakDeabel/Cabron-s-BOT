7afadb7f5f9a4b41dc2fcfee09a9b1dbaef933c6
Glitch (low-flying-son) <none>
2020-07-23 06:15:13 +0000

    👕🚄 Updated with Glitch

--- index.js
+++ index.js
@@ -11,35 +11,6 @@ const bot = new Discord.Client({disableEveryone: false});
 bot.commands = new Discord.Collection();
 bot.aliases = new Discord.Collection();
 
-bot.on("message", async message => {
-  if(message.content === "c!profile") {
-  
-    const avatar = await fetch(message.author.avatarURL({format: 'jpg'}))
-    
-    
-    
-let mage = new Canvas(500, 250)
-.setColor("#ffffff")
-.addRect(0, 0, 500, 250) //we gonna replace it with image
-.setColor("#ff2050")
-.addRect(0, 0, 500, 80)
-.setColor("#ffffff")
-.setTextFont('bold 40px Impact') //you can make it bold
-.addText("PROFILE CARD", 110, 55)
-.setColor("#ff2050")
-.setTextFont('bold 20px Impact') 
-.addText(`ID - ${message.author.id}`, 30, 140)
-.addText(`TAG - ${message.author.tag}`, 30, 170)
-.addText(`GUILD NAME - ${message.guild.name}`, 30, 200)
-.setColor("#ffffff")
-.addCircle(60, 40, 33)
-.addCircularImage(await avatar.buffer(), 60, 40, 30)
-.toBuffer();
-    
-    message.channel.send({files: [mage]}) //lol i forget again
-    
-  }
-  })//THESE CODE
 
 bot.on('guildMemberAdd', member => {
   
